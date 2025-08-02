05cd962bef4a942790cdd6d3e472f29f27623478
satyam Kumar <satyam1430system@gmail.com>
2025-08-02 14:49:44 +0530

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup=async(req,res)=>{
             if(err){
                 return next(err);
             }
-             req.flash("success","Welcome to Wanderlust!");
+             req.flash("success","Welcome to StayYatra!");
             res.redirect("/listings")
         });
        
